087349f9a8583758241bd808dd22d5131863e148
TwilightLogic <twilightlogic@gmail.com>
2023-05-08 17:03:05 +0800

    Added prettier format

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -9,8 +9,6 @@ function App() {
   const [tours, setTours] = useState([]);
 
   const fetchTours = async () => {
-    setLoading(true);
-
     try {
       const response = await fetch(url);
       const tours = await response.json();
