07facd2c298dd8965b4d5c2129c16078359ffeba
TwilightLogic <twilightlogic@gmail.com>
2023-06-02 15:01:25 +0800

    Display the first item of the data from API

--- 06-tabs/setup/src/App.js
+++ 06-tabs/setup/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
+
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tabs-project";
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [jobs, setJobs] = useState([]);
@@ -34,6 +36,23 @@ function App() {
       <div className="title">
         <h2>experience</h2>
         <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* job container */}
+        {/* job info */}
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
       </div>
     </section>
   );
